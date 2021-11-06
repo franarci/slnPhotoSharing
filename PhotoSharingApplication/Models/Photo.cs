8b44f284f85e2bbb9fffdeb017c4efae561e127d
franarci <fran.arciniega96@gmail.com>
2021-11-06 12:36:55 -0300

    Photo

---
+++ PhotoSharingApplication/Models/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models
+{
+    public class Photo
+    {
+        public int PhotoID { get; set; }
+        public string Title { get; set; }
+        public byte[] PhotoFile{get; set; }
+        public string ImageMimeType { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate{ get; set; }
+        public string UserName { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+
+}
