8debf9da5d0229732cee77b0bdcf941b72db00b4
franarci <fran.arciniega96@gmail.com>
2021-11-07 19:55:13 -0300

    _PhotoGallery y cambio Index

--- PhotoSharingApplication/Controllers/PhotoController.cs
+++ PhotoSharingApplication/Controllers/PhotoController.cs
@@ -17,9 +17,27 @@ namespace PhotoSharingApplication.Controllers
         // GET: Photo
         public ActionResult Index()
         {
-            return View("Index", context.Photos.ToList());
+            return View("Index");
 
         }
+
+        [ChildActionOnly]
+        public ActionResult _PhotoGallery(int number = 0)
+        {
+            List<Photo> photos;
+            if (number == 0)
+            {
+                photos = context.Photos.ToList();
+            }
+            else
+            {
+                photos = (
+                from p in context.Photos
+                orderby p.CreatedDate descending select p).Take(number).ToList();
+            }
+            return PartialView("_PhotoGallery", photos);
+        }
+
         public ActionResult Display(int id)
         {
             Photo photo = context.Photos.Find(id);
